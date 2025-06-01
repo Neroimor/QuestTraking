d792c5af29c72b136f17831c1374de71c3295e58
Neroimor <neroimor@gmail.com>
2025-06-01 20:40:16 +0300

    added swagger

--- QuestTrakingAPI/Program.cs
+++ QuestTrakingAPI/Program.cs
@@ -1,3 +1,4 @@
+using Microsoft.AspNetCore.Builder;
 using Microsoft.EntityFrameworkCore;
 using QuestTrakingAPI.DataBase.Services;
 
@@ -8,6 +9,7 @@ var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddControllers();
 builder.Services.AddLogging();
 builder.Services.AddOpenApi();
+builder.Services.AddSwaggerGen();
 var connectionString = builder.Configuration.GetConnectionString("DefaultConnection");
 builder.Services.AddDbContext<AppDBContext>(options =>
     options.UseNpgsql(connectionString));
@@ -18,6 +20,12 @@ var app = builder.Build();
 if (app.Environment.IsDevelopment())
 {
     app.MapOpenApi();
+    app.UseSwagger();
+
+    app.UseSwaggerUI(options =>
+    {
+        options.SwaggerEndpoint("v1/swagger.json", "My API V1");
+    });
 }
 
 app.UseHttpsRedirection();
