fd66df5f825babdae4b62fed119920e8d9c65a0c
Neroimor <neroimor@gmail.com>
2025-06-01 19:38:31 +0300

    added database

---
+++ QuestTrakingAPI/DataBase/DTO/User.cs
@@ -0,0 +1,17 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace QuestTrakingAPI.DataBase.DTO
+{
+    public class User
+    {
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public int Id { get; set; }
+        [Required, StringLength(50)]
+        public string Name { get; set; } = string.Empty;
+        public DateTime CreatedAt { get; set; } = DateTime.UtcNow;
+        public List<Quest> Quests { get; set; } = new List<Quest>();
+  
+    }
+}
