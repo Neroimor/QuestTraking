efe189b2404bca739fea33cae7af9c06205761ff
Neroimor <neroimor@gmail.com>
2025-06-09 09:46:59 +0300

    update ui

---
+++ QuestFront/DTO/RequestUser.cs
@@ -0,0 +1,12 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace QuestTrakingAPI.DataBase.DTO
+{
+    public class RequestUser
+    {
+        [Required, StringLength(50)]
+        public string Name { get; set; } = string.Empty;
+        [Required, EmailAddress]
+        public string Email { get; set; } = string.Empty;
+    }
+}
